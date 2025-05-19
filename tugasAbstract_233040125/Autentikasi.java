23b463cb5fca3d227eec9c2bd915eeb77a6461fc
mildakhaerunnisa <146157113+mildakhaerunnisa@users.noreply.github.com>
2025-05-19 21:58:38 +0700

    Delete tugasAbstract_233040125 directory

--- tugasAbstract_233040125/Autentikasi.java
+++
@@ -1,6 +0,0 @@
-package tugasAbstract_233040125;
-
-public interface Autentikasi {
-	boolean login (String pin);
-
-}
--- tugasAbstract_233040125/Kartu.java
+++
@@ -1,6 +0,0 @@
-package tugasAbstract_233040125;
-
-public interface Kartu {
-    public boolean otentikasi(String pin);
-    public String encode(String pin);
-}
